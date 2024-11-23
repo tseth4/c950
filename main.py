8668bb7ce5749acebf33300213fdb4b470e04dc2
ts <tristansetha@gmail.com>
2024-11-22 21:44:21 -0800

    update

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# C950 WGUPS ROUTING
+# By: Tristan Setha
+# Student ID: 012068201
+
+
 from src.data_loader import load_packages, load_distances
 from src.truck import Truck
 from src.package import Package
@@ -11,15 +16,14 @@ from datetime import datetime, timedelta
 # Delayed on flight---will not arrive to depot until <9:05 am>
 # Package 9 Wrong address listed
 # Must be delivered with n, n
-# 
 
+# TODO:
+# D.  Provide an intuitive interface for the user to view the delivery status (including the delivery time) of any package at any time and the total mileage traveled by all trucks. (The delivery status should report the package as at the hub, en route, or delivered. Delivery status must include the time.)
+# 1.  Provide screenshots to show the status of all packages loaded onto each truck at a time between 8:35 a.m. and 9:25 a.m.
+# 2.  Provide screenshots to show the status of all packages loaded onto each truck at a time between 9:35 a.m. and 10:25 a.m.
+# 3.  Provide screenshots to show the status of all packages loaded onto each truck at a time between 12:03 p.m. and 1:12 p.m.
+# E.  Provide screenshots showing successful completion of the code that includes the total mileage traveled by all trucks.
 
-"""
-Deliver 40 packages using 3 trucks under 140 miles total.
-Meet package-specific constraints (deadlines, delivery address corrections).
-Output delivery progress for packages (time delivered, status, etc.).
-Write clean, modular, and maintainable code.
-"""
 
 # packages as HashMap
 packages = load_packages()
@@ -28,25 +32,31 @@ packages = load_packages()
 # distances as AdjacencyMatrix
 addresses, distances = load_distances()
 
+# Set package address_index's
 for p in packages.values():
     p.set_address_index(addresses.index(p.get_address()))
 
-# Truck class
+# Truck classes
 truck1 = Truck(id=1, addresses=addresses, capacity=16)
 truck2 = Truck(id=2, addresses=addresses, capacity=16)
 
+# Truck array
 trucks = [truck1, truck2]
+
+# Sort all packages by deadline
 sorted_packages = packages.get_sorted_packages_by_deadline()
 
+# Assign packages to each truck
 for i, package in enumerate(sorted_packages):
     trucks[i % len(trucks)].assign_package(package)
 
 
+# Start delivery process
 for truck in trucks:
   truck.process_deliveries(distances.matrix
                           #  , datetime.strptime("9:00:00", "%H:%M:%S")
                            )
 
-
+# Print delivery data
 for package in packages.values():
     print(package)
